03e1f301f9f184e88e31eba41bf5769ea87e9885
danadomi <lopeznarvaez.65@gmail.com>
2017-10-06 15:57:34 -0500

    Agregando el controlador de persona juridica

---
+++ MantenimientoPersonasSVS/MantenimientoPersonasSVS/Controllers/PJuridicaController.cs
@@ -0,0 +1,78 @@
+﻿
+using MantenimientoPersonasSVS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MantenimientoPersonasSVS.Controllers
+{
+    public class PJuridicaController : Controller
+    {
+        //
+        // GET: /PJuridica/
+
+        public ActionResult ListaPersonaJuridica()
+        {
+            PJuridica persona = new PJuridica();
+            ViewData["listaPJuridica"] = persona.listar(persona);
+
+            return View();
+        }
+        public ActionResult Crear() { 
+         
+            //muestra el formulario
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Crear(FormCollection datos)
+        {
+            PJuridica persona = new PJuridica();
+
+            persona.ruc = datos["ruc"];
+            persona.ruc = datos["razonSocial"];
+            persona.ruc = datos["paginaURL"];
+
+            //muestra el formulario
+            persona.insertar();
+            return View();
+        }
+        public ActionResult Editar()
+        {
+
+            //muestra el formulario
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Editar(FormCollection datos)
+        {
+            //sobre carga de inyeccion de datos 
+            PJuridica persona = new PJuridica();
+            persona.ruc = datos["ruc"];
+            persona.ruc = datos["razonSocial"];
+            persona.ruc = datos["paginaURL"];
+
+            //muestra el formulario
+            persona.editar();
+            return View();
+        }
+        public ActionResult Eliminar()
+        {
+            //muestra el formulario
+
+            return View();
+        }
+         [HttpPost]
+        public ActionResult Eliminar(FormCollection datos)
+        {
+            //muestra el formulario
+            PJuridica persona = new PJuridica();
+            persona.ruc = datos["ruc"];
+            persona.eliminar();
+            return View();
+        }
+
+       
+    }
+}
